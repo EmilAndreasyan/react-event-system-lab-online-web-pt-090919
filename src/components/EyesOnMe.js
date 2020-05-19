7f7fc144ef7616db92d1f7f586f2c842a61ceb37
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-19 03:00:50 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,14 +1,19 @@
 // Code EyesOnMe Component Here
-import React, {Component} from 'react'
+import React from 'react';
 
-export default class EyesOnMe extends Component {
-  
-  render(){
-     return (
-       <button onFocus={this.focusing} onBlur={this.bluring}>Focus</button>
-       )
-   }
-  bluring = () => console.log("Good!")
-  
-  focusing = () => console.log("Hey! Eyes on me!")
+class EyesOnMe extends React.Component {
+
+  onFocus = () => console.log('Good!')
+
+  onBlur = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return (
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
+      </button>
+    )
+  }
 }
+
+export default EyesOnMe;
