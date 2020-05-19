2bbec1316f31aa9201ac8c4917d0d51c90acd4e3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-19 02:57:49 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,14 +1,9 @@
 // Code Keypad Component Here
-<<<<<<< HEAD
 import React, {Component} from 'react'
-=======
-import React {Component} from 'react'
->>>>>>> a69363db702ecb7e65e08ed28e33fccc2f5f100a
 
 export default class Keypad extends Component {
   render(){
     return (
-<<<<<<< HEAD
       <input onKeyUp={this.enterPassword} type="password" />
       )
   }
@@ -17,12 +12,7 @@ export default class Keypad extends Component {
     console.log('Entering password...')
   }
   
-=======
-      <input onKeyUp={console.log('Entering password')} type="password" />
-      )
-  }
-  
->>>>>>> a69363db702ecb7e65e08ed28e33fccc2f5f100a
+
   
   
 }
